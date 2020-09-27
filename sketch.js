54999a9ad33442793402d72eae58037dc3a4ea5d
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-09-27 01:39:55 -0700

    121

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+
+let dogs = [{name: "(￣▽￣)", color: "one"},{name: "（＞人＜；）", color: "two"},{name: "(。・∀・)", color:"three"},{name: "(❤ ω ❤)", color:"four"},{name: "(～﹃～)", color:"five"},{name: "( $ _ $ )", color:"six"}];
+let randomIndex;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+    background(200);
+    textSize(32);
+    text("click",50,50);
+
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed(){
+  if (dogs[0]){
+  background(random(200, 255));
+  randomIndex = int(random(dogs.length));
+  //console.log(dogs[randomIndex].name);
+  text(dogs[randomIndex].name, 50, 50);
+
+  dogs.splice(randomIndex, 1);
+
+} else {
+  background(random(200, 255));
+  text("END",50,50);
+}
 }
