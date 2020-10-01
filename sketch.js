2d2352caba2a18227b4f622a9bc9ed542897cd3d
chengyuanshi <chengyuan.shi@sjsu.edu>
2020-09-30 18:31:44 -0700

    111

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 
-let dogs = [{name: "(￣▽￣)", color: "one"},{name: "（＞人＜；）", color: "two"},{name: "(。・∀・)", color:"three"},{name: "(❤ ω ❤)", color:"four"},{name: "(～﹃～)", color:"five"},{name: "( $ _ $ )", color:"six"}];
+let emoji = [{name: "(￣▽￣)", color: "How are you today?"},{name: "（＞人＜；）", color: "How are you yesterday?"},{name: "(。・∀・)", color:"How are you on Sunday?"},{name: "(❤ ω ❤)", color:"How are you on Sunday?"},{name: "(～﹃～)", color:"How are you in the morning?"},{name: "( $ _ $ )", color:"Did you sleep well last night?"}];
 let randomIndex;
+let counter = 0;
+let animating = false;
 
 function setup() {
   createCanvas(600, 600);
@@ -8,24 +10,48 @@ function setup() {
     textSize(32);
     text("click",50,50);
 
+    // setTimeout(changeBackground, 1000);
+
 
 }
 
 function draw() {
- 
+
+if(animating == true){
+  ellipse(random(width), random(height), random(50,200));
 }
+}
+
+function randomizer(){
+    animating = false;
+    if (emoji[0]){
+    background(random(200, 255));
+    randomIndex = int(random(emoji.length));
+    //console.log(emoji[randomIndex].name);
+    text(`${emoji[randomIndex].name} Hello!
+${emoji[randomIndex].color}`, 50, 50);
+    // text(emoji[randomIndex].name + "Hello!" + emoji[randomIndex].color, 50, 50);
+
+    emoji.splice(randomIndex, 1);
+
+  } else {
+    background(random(200, 255));
+    text("END",50,50);
+  }
+}
+// function changeBackground(){
+//   if (counter <= 5){
+//     counter++;
+//     console.log(counter)
+//   background(random(255), random(255), random(255));
+//   setTimeout(changeBackground, 1000);
+// } else{
+//
+// }
+// }
 
 function mousePressed(){
-  if (dogs[0]){
-  background(random(200, 255));
-  randomIndex = int(random(dogs.length));
-  //console.log(dogs[randomIndex].name);
-  text(dogs[randomIndex].name, 50, 50);
+  animating = true;
+  setTimeout(randomizer, 2000);
 
-  dogs.splice(randomIndex, 1);
-
-} else {
-  background(random(200, 255));
-  text("END",50,50);
-}
 }
